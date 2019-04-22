ffce7f84bafba8018c5d012644706998282d7634
Daniel Jurczak <danieljur4@gmail.com>
2019-04-22 16:03:41 +0200

    Working bot

--- code.py
+++ code.py
@@ -2,6 +2,8 @@ import PIL.ImageGrab as ImageGrab
 import os
 import time
 import win32api, win32con
+import PIL.ImageOps as ImageOps
+from numpy import *
 """
  
 All coordinates assume a screen resolution of 2560x1080, and Chrome 
@@ -16,13 +18,29 @@ Play area =  x_pad+1, y_pad+1, 959, 623
  
 x_pad = 321
 y_pad = 144
+foodOnHand = {'shrimp':5,
+              'rice':10,
+              'nori':10,
+              'roe':10,
+              'salmon':5,
+              'unagi':5}
+sushiTypes = {2670:'onigiri', 
+              3143:'caliroll',
+              2677:'gunkan'}
+class Blank:
+    seat_1 = 8119
+    seat_2 = 5986
+    seat_3 = 11596
+    seat_4 = 10613
+    seat_5 = 7286
+    seat_6 = 9119
 
 class Cord:
      
     f_shrimp = (33, 330)
     f_rice = (91, 333)
     f_nori = (22, 392)
-    f_roe = (124, 388)
+    f_roe = (91, 388)
     f_salmon = (14, 441)
     f_unagi = (99, 435)
 
@@ -43,39 +61,33 @@ class Cord:
     buy_rice = (546, 272)
      
     delivery_norm = (498, 292)
-"""
-Plate cords: 
-    45 207
-    141 204
-    245 208
-    340 207
-    440 215
-    546 213
 
-"""
 def clear_tables():
-    mousePos((45, 207))
+    mousePos((75, 207))
     leftClick()
  
-    mousePos((141, 204))
+    mousePos((171, 204))
     leftClick()
  
-    mousePos((245, 208))
+    mousePos((275, 208))
     leftClick()
  
-    mousePos((340, 207))
+    mousePos((370, 207))
     leftClick()
  
-    mousePos((440, 215))
+    mousePos((480, 205))
     leftClick()
  
-    mousePos((546, 213))
+    mousePos((580, 213))
     leftClick()
     time.sleep(1)
 
 def makeFood(food):
     if food == 'caliroll':
         print('Making a caliroll')
+        foodOnHand['rice'] -= 1
+        foodOnHand['nori'] -= 1
+        foodOnHand['roe'] -= 1 
         mousePos(Cord.f_rice)
         leftClick()
         time.sleep(.05)
@@ -90,6 +102,8 @@ def makeFood(food):
      
     elif food == 'onigiri':
         print('Making a onigiri')
+        foodOnHand['rice'] -= 2 
+        foodOnHand['nori'] -= 1 
         mousePos(Cord.f_rice)
         leftClick()
         time.sleep(.05)
@@ -105,6 +119,10 @@ def makeFood(food):
         time.sleep(1.5)
  
     elif food == 'gunkan':
+        print('Making a gunkan')
+        foodOnHand['rice'] -= 1 
+        foodOnHand['nori'] -= 1 
+        foodOnHand['roe'] -= 2 
         mousePos(Cord.f_rice)
         leftClick()
         time.sleep(.05)
@@ -119,6 +137,162 @@ def makeFood(food):
         time.sleep(.1)
         foldMat()
         time.sleep(1.5)
+
+def buyFood(food):
+     
+    if food == 'rice':
+        mousePos(Cord.phone)
+        time.sleep(.1)
+        leftClick()
+        mousePos(Cord.menu_rice)
+        time.sleep(.05)
+        leftClick()
+        s = screenGrab()
+        if s.getpixel(Cord.buy_rice) == (225, 181, 105):
+            print('rice is available')
+            mousePos(Cord.buy_rice)
+            time.sleep(.1)
+            leftClick()
+            mousePos(Cord.delivery_norm)
+            foodOnHand['rice'] += 10 
+            time.sleep(.1)
+            leftClick()
+            time.sleep(2.5)
+        else:
+            print('rice is NOT available')
+            mousePos(Cord.t_exit)
+            leftClick()
+            time.sleep(1)
+            buyFood(food)
+             
+ 
+             
+    if food == 'nori':
+        mousePos(Cord.phone)
+        time.sleep(.1)
+        leftClick()
+        mousePos(Cord.menu_toppings)
+        time.sleep(.05)
+        leftClick()
+        s = screenGrab()
+        print('test')
+        time.sleep(.1)
+        if s.getpixel(Cord.t_nori) != (108, 108, 79):
+            print('nori is available')
+            mousePos(Cord.t_nori)
+            time.sleep(.1)
+            leftClick()
+            mousePos(Cord.delivery_norm)
+            foodOnHand['nori'] += 10
+            time.sleep(.1)
+            leftClick()
+            time.sleep(2.5)
+        else:
+            print('nori is NOT available')
+            mousePos(Cord.t_exit)
+            leftClick()
+            time.sleep(1)
+            buyFood(food)
+ 
+    if food == 'roe':
+        mousePos(Cord.phone)
+        time.sleep(.1)
+        leftClick()
+        mousePos(Cord.menu_toppings)
+        time.sleep(.05)
+        leftClick()
+        s = screenGrab()
+         
+        time.sleep(.1)
+        if s.getpixel(Cord.t_roe) != (255, 252, 172):
+            print('roe is available')
+            mousePos(Cord.t_roe)
+            time.sleep(.1)
+            leftClick()
+            mousePos(Cord.delivery_norm)
+            foodOnHand['roe'] += 10 
+            time.sleep(.1)
+            leftClick()
+            time.sleep(2.5)
+        else:
+            print('roe is NOT available')
+            mousePos(Cord.t_exit)
+            leftClick()
+            time.sleep(1)
+            buyFood(food)
+def checkFood():
+    for i, j in foodOnHand.items():
+        if i == 'nori' or i == 'rice' or i == 'roe':
+            if j <= 4:
+                print('%s is low and needs to be replenished' % i)
+                buyFood(i)
+
+def get_seat_one():
+    box = (x_pad+24,y_pad+60,x_pad+24+63,y_pad+60+16)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print(a)
+    im.save(os.getcwd() + '\\seat_one__' + str(int(time.time())) + '.png', 'PNG')    
+    return a
+ 
+def get_seat_two():
+    box = (x_pad+125,y_pad+60,x_pad+125+63,y_pad+60+16)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print(a)
+    im.save(os.getcwd() + '\\seat_two__' + str(int(time.time())) + '.png', 'PNG')    
+    return a
+ 
+def get_seat_three():
+    box = (x_pad+226,y_pad+60,x_pad+226+63,y_pad+60+16)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print(a)
+    im.save(os.getcwd() + '\\seat_three__' + str(int(time.time())) + '.png', 'PNG')    
+    return a
+ 
+def get_seat_four():
+    box = (x_pad+327,y_pad+60,x_pad+327+63,y_pad+60+16)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print(a)
+    im.save(os.getcwd() + '\\seat_four__' + str(int(time.time())) + '.png', 'PNG')    
+    return a
+ 
+def get_seat_five():
+    box = (x_pad+428,y_pad+60,x_pad+428+63,y_pad+60+16)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print(a)
+    im.save(os.getcwd() + '\\seat_five__' + str(int(time.time())) + '.png', 'PNG')    
+    return a
+ 
+def get_seat_six():
+    box = (x_pad+529,y_pad+60,x_pad+529+63,y_pad+60+16)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print(a)
+    im.save(os.getcwd() + '\\seat_six__' + str(int(time.time())) + '.png', 'PNG')    
+    return a
+ 
+def get_all_seats():
+    get_seat_one()
+    get_seat_two()
+    get_seat_three()
+    get_seat_four()
+    get_seat_five()
+    get_seat_six()
+
+def foldMat():
+    mousePos((Cord.f_rice[0]+40,Cord.f_rice[1])) 
+    leftClick()
+    time.sleep(.1)
 
 def mousePos(cord):
     win32api.SetCursorPos((x_pad + cord[0],y_pad + cord[1]))
@@ -146,10 +320,20 @@ def get_cords():
     print(x,y)
 
 def screenGrab():
-    box = (x_pad+1, y_pad+1, x_pad+640, y_pad+479)
+    box = (x_pad + 1,y_pad+1,x_pad+640,y_pad+480)
     im = ImageGrab.grab(box)
-    im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
-    '.png', 'PNG')
+ 
+    ##im.save(os.getcwd() + '\\Snap__' + str(int(time.time())) + '.png', 'PNG')
+    return im
+     
+     
+def grab():
+    box = (x_pad + 1,y_pad+1,x_pad+640,y_pad+480)
+    im = ImageOps.grayscale(ImageGrab.grab(box))
+    a = array(im.getcolors())
+    a = a.sum()
+    print(a)
+    return a
 
 def startGame():
     #location of first menu
@@ -172,8 +356,89 @@ def startGame():
     leftClick()
     time.sleep(.1)
 
+def check_bubs():
+ 
+    checkFood()
+    s1 = get_seat_one()
+    if s1 != Blank.seat_1:
+        if sushiTypes.has_key(s1):
+            print('table 1 is occupied and needs %s' % sushiTypes[s1])
+            makeFood(sushiTypes[s1])
+        else:
+            print('sushi not found!\n sushiType = %i' % s1)
+ 
+    else:
+        print('Table 1 unoccupied')
+ 
+    clear_tables()
+    checkFood()
+    s2 = get_seat_two()
+    if s2 != Blank.seat_2:
+        if sushiTypes.has_key(s2):
+            print('table 2 is occupied and needs %s' % sushiTypes[s2])
+            makeFood(sushiTypes[s2])
+        else:
+            print('sushi not found!\n sushiType = %i' % s2)
+ 
+    else:
+        print('Table 2 unoccupied')
+ 
+    checkFood()
+    s3 = get_seat_three()
+    if s3 != Blank.seat_3:
+        if sushiTypes.has_key(s3):
+            print('table 3 is occupied and needs %s' % sushiTypes[s3])
+            makeFood(sushiTypes[s3])
+        else:
+            print('sushi not found!\n sushiType = %i' % s3)
+ 
+    else:
+        print('Table 3 unoccupied')
+ 
+    checkFood()
+    s4 = get_seat_four()
+    if s4 != Blank.seat_4:
+        if sushiTypes.has_key(s4):
+            print('table 4 is occupied and needs %s' % sushiTypes[s4])
+            makeFood(sushiTypes[s4])
+        else:
+            print('sushi not found!\n sushiType = %i' % s4)
+ 
+    else:
+        print('Table 4 unoccupied')
+ 
+    clear_tables()
+    checkFood()
+    s5 = get_seat_five()
+    if s5 != Blank.seat_5:
+        if sushiTypes.has_key(s5):
+            print('table 5 is occupied and needs %s' % sushiTypes[s5])
+            makeFood(sushiTypes[s5])
+        else:
+            print('sushi not found!\n sushiType = %i' % s5)
+ 
+    else:
+        print('Table 5 unoccupied')
+ 
+    checkFood()
+    s6 = get_seat_six()
+    if s6 != Blank.seat_6:
+        if sushiTypes.has_key(s6):
+            print('table 1 is occupied and needs %s' % sushiTypes[s6])
+            makeFood(sushiTypes[s6])
+        else:
+            print('sushi not found!\n sushiType = %i' % s6)
+ 
+    else:
+        print('Table 6 unoccupied')
+ 
+    clear_tables()
+
+
 def main():
-    pass
+    startGame()
+    while True:
+        check_bubs()
 
  
 if __name__ == '__main__':
